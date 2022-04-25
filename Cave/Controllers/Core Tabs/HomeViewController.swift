7a17897e2af71dafc481dbc8575f39f794281c52
mg6613 <mg6613@naver.com>
2022-04-25 20:15:24 +0900

    Create Cotroller and Tab bar

---
+++ Cave/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cave
+//
+//  Created by 유민규 on 2022/02/18.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cave/Controllers/Core Tabs/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Cave
+//
+//  Created by 유민규 on 2022/02/18.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
